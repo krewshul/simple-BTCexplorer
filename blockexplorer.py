e5900058809af78d155829479ee3ca8b12a50726
jeremy lee <31496594+krewshul@users.noreply.github.com>
2023-02-20 19:04:55 -0600

    Update and rename blockexplorer4.py to blockexplorer.py
    This version removes the get_input method, simplifies the create_input_section method, and eliminates the need for a separate get_x_info method for each search type. Instead, the create_input_section method takes a URL suffix and creates a button that retrieves the JSON data for the specified URL when clicked. The JSON data is then passed to the display_json method, which creates a separate window to display the results.

--- blockexplorer.py
+++ blockexplorer.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 import requests
 
-
 class BlockchainInfoAPI:
     BASE_URL = "https://blockchain.info"
 
@@ -10,54 +9,35 @@ class BlockchainInfoAPI:
         response = requests.get(url)
         return response.json()
 
-
 class BlockSearchWindow:
     def __init__(self, master):
         self.master = master
-        self.entry_widget = None
         master.title("BTC EXPLORER")
         master.geometry("400x225")
         master.configure(bg='black')
 
         # Create the block hash search section
-        self.block_hash_entry = self.create_input_section("Enter block hash:", "Get Block Info", self.get_block_info)
+        self.block_hash_entry = self.create_input_section("Enter block hash:", "Get Block Info", "rawblock/{}")
 
         # Create the transaction hash search section
-        self.tx_hash_entry = self.create_input_section("Enter transaction hash:", "Get Transaction Info", self.get_tx_info)
+        self.tx_hash_entry = self.create_input_section("Enter transaction hash:", "Get Transaction Info", "rawtx/{}")
 
         # Create the address search section
-        self.address_entry = self.create_input_section("Enter address:", "Get Address Info", self.get_address_info)
+        self.address_entry = self.create_input_section("Enter address:", "Get Address Info", "rawaddr/{}")
 
-    def create_input_section(self, label_text, button_text, command_func):
+    def create_input_section(self, label_text, button_text, url_suffix):
         label = tk.Label(self.master, text=label_text, fg="white", bg="black")
         label.pack()
         entry = tk.Entry(self.master, width=40)
         entry.pack()
-        self.entry_widget = entry
-        button = tk.Button(self.master, text=button_text, command=command_func, fg="white", bg="black")
+        button = tk.Button(self.master, text=button_text, command=lambda: self.get_info(url_suffix.format(entry.get())), fg="white", bg="black")
         button.pack()
         return entry
 
-    def get_block_info(self):
-        block_hash = self.block_hash_entry.get()
+    def get_info(self, url_suffix):
         api = BlockchainInfoAPI()
-        block_info = api.get_info(f"rawblock/{block_hash}")
-        self.display_json(block_info)
-
-    def get_tx_info(self):
-        tx_hash = self.tx_hash_entry.get()
-        api = BlockchainInfoAPI()
-        tx_info = api.get_info(f"rawtx/{tx_hash}")
-        self.display_json(tx_info)
-
-    def get_address_info(self):
-        address = self.address_entry.get()
-        api = BlockchainInfoAPI()
-        address_info = api.get_info(f"rawaddr/{address}")
-        self.display_json(address_info)
-
-    def get_input(self):
-        return self.entry_widget.get()
+        json_data = api.get_info(url_suffix)
+        self.display_json(json_data)
 
     def display_json(self, json_data):
         top = tk.Toplevel(self.master)
@@ -77,7 +57,6 @@ class BlockSearchWindow:
                        "result", "time", "height", "main_chain", "mrkl_root", "n_tx", "next_block",
                        "prev_block", "address", "total_received", "total_sent", "final_balance"]:
                 text.insert("1.0", f"{key.title().replace('_', ' ')}: {value}\n")
-
 
 if __name__ == "__main__":
     root = tk.Tk()
