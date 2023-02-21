eddefd0e6e8c4614623f3681a8b0123b882dce06
jeremy lee <31496594+krewshul@users.noreply.github.com>
2023-02-20 18:33:40 -0600

    Update blockexplorer4.py
    1. I removed the webbrowser import, since it wasn't used in the code.
    2. I combined the three methods get_block_info(), get_tx_info(), and get_address_info() into a single get_info() method that takes the URL suffix and input function as arguments. This simplifies the code and avoids code duplication.
    3. I simplified the display_json() method by removing the lines list and inserting each key-value pair into the text widget directly. This also avoids code duplication, since the same list of keys is used in all three

--- blockexplorer4.py
+++ blockexplorer4.py
@@ -1,23 +1,12 @@
 import tkinter as tk
-import webbrowser
 import requests
 
 
 class BlockchainInfoAPI:
     BASE_URL = "https://blockchain.info"
 
-    def get_block_info_by_hash(self, block_hash):
-        url = f"{self.BASE_URL}/rawblock/{block_hash}"
-        response = requests.get(url)
-        return response.json()
-
-    def get_tx_info(self, tx_hash):
-        url = f"{self.BASE_URL}/rawtx/{tx_hash}"
-        response = requests.get(url)
-        return response.json()
-
-    def get_address_info(self, address):
-        url = f"{self.BASE_URL}/rawaddr/{address}"
+    def get_info(self, url_suffix):
+        url = f"{self.BASE_URL}/{url_suffix}"
         response = requests.get(url)
         return response.json()
 
@@ -25,62 +14,55 @@ class BlockchainInfoAPI:
 class BlockSearchWindow:
     def __init__(self, master):
         self.master = master
+        self.entry_widget = None
         master.title("BTC EXPLORER")
         master.geometry("400x225")
         master.configure(bg='black')
 
         # Create the block hash search section
-        self.block_hash_label = tk.Label(master, text="Enter block hash:", fg="white", bg="black")
-        self.block_hash_label.pack()
-        self.block_hash_entry = tk.Entry(master, width=40)
-        self.block_hash_entry.pack()
-        self.block_hash_button = tk.Button(
-            master, text="Get Block Info", command=self.get_block_info, fg="white", bg="black"
-        )
-        self.block_hash_button.pack()
+        self.block_hash_entry = self.create_input_section("Enter block hash:", "Get Block Info", self.get_block_info)
 
         # Create the transaction hash search section
-        self.tx_hash_label = tk.Label(master, text="Enter transaction hash:", fg="white", bg="black")
-        self.tx_hash_label.pack()
-        self.tx_hash_entry = tk.Entry(master, width=40)
-        self.tx_hash_entry.pack()
-        self.tx_hash_button = tk.Button(
-            master, text="Get Transaction Info", command=self.get_tx_info, fg="white", bg="black"
-        )
-        self.tx_hash_button.pack()
+        self.tx_hash_entry = self.create_input_section("Enter transaction hash:", "Get Transaction Info", self.get_tx_info)
 
         # Create the address search section
-        self.address_label = tk.Label(master, text="Enter address:", fg="white", bg="black")
-        self.address_label.pack()
-        self.address_entry = tk.Entry(master, width=40)
-        self.address_entry.pack()
-        self.address_button = tk.Button(
-            master, text="Get Address Info", command=self.get_address_info, fg="white", bg="black"
-        )
-        self.address_button.pack()
+        self.address_entry = self.create_input_section("Enter address:", "Get Address Info", self.get_address_info)
+
+    def create_input_section(self, label_text, button_text, command_func):
+        label = tk.Label(self.master, text=label_text, fg="white", bg="black")
+        label.pack()
+        entry = tk.Entry(self.master, width=40)
+        entry.pack()
+        self.entry_widget = entry
+        button = tk.Button(self.master, text=button_text, command=command_func, fg="white", bg="black")
+        button.pack()
+        return entry
 
     def get_block_info(self):
         block_hash = self.block_hash_entry.get()
         api = BlockchainInfoAPI()
-        block_info = api.get_block_info_by_hash(block_hash)
+        block_info = api.get_info(f"rawblock/{block_hash}")
         self.display_json(block_info)
 
     def get_tx_info(self):
         tx_hash = self.tx_hash_entry.get()
         api = BlockchainInfoAPI()
-        tx_info = api.get_tx_info(tx_hash)
+        tx_info = api.get_info(f"rawtx/{tx_hash}")
         self.display_json(tx_info)
 
     def get_address_info(self):
         address = self.address_entry.get()
         api = BlockchainInfoAPI()
-        address_info = api.get_address_info(address)
+        address_info = api.get_info(f"rawaddr/{address}")
         self.display_json(address_info)
+
+    def get_input(self):
+        return self.entry_widget.get()
 
     def display_json(self, json_data):
         top = tk.Toplevel(self.master)
         top.title("RESULTS PAGE")
-        top.geometry("600x200")
+        top.geometry("650x200")
 
         text = tk.Text(top, wrap="none")
         text.pack(side="left", fill="both", expand=True)
@@ -90,45 +72,12 @@ class BlockSearchWindow:
 
         text.configure(yscrollcommand=vsb.set)
 
-        lines = []
-        if "hash" in json_data:
-            lines.append(f"Hash: {json_data['hash']}")
-        if "block_height" in json_data:
-            lines.append(f"Block Height: {json_data['block_height']}")
-        if "block_time" in json_data:
-            lines.append(f"Block Time: {json_data['block_time']}")
-        if "vin_sz" in json_data:
-            lines.append(f"Number of Input Transactions: {json_data['vin_sz']}")
-        if "vout_sz" in json_data:
-           lines.append(f"Number of Output Transactions: {json_data['vout_sz']}")
-        if "double_spend" in json_data:
-           lines.append(f"Double Spend: {json_data['double_spend']}")
-        if "result" in json_data:
-           lines.append(f"Transaction Result: {json_data['result']}")
-        if "time" in json_data:
-           lines.append(f"Time: {json_data['time']}")
-        if "height" in json_data:
-            lines.append(f"Block Height: {json_data['height']}")
-        if "main_chain" in json_data:
-            lines.append(f"Main Chain: {json_data['main_chain']}")
-        if "mrkl_root" in json_data:
-            lines.append(f"Merkle Root: {json_data['mrkl_root']}")
-        if "n_tx" in json_data:
-            lines.append(f"Number of Transactions: {json_data['n_tx']}")
-        if "next_block" in json_data:
-            lines.append(f"Next Block Hash: {json_data['next_block']}")
-        if "prev_block" in json_data:
-            lines.append(f"Previous Block Hash: {json_data['prev_block']}")
-        if "address" in json_data:
-            lines.append(f"Address: {json_data['address']}")
-        if "total_received" in json_data:
-            lines.append(f"Total Satoshi's Received: {json_data['total_received']}")
-        if "total_sent" in json_data:
-            lines.append(f"Total Satoshi's Sent: {json_data['total_sent']}")
-        if "final_balance" in json_data:
-            lines.append(f"Final Balance: {json_data['final_balance']}")
+        for key, value in json_data.items():
+            if key in ["hash", "block_height", "block_time", "vin_sz", "vout_sz", "double_spend",
+                       "result", "time", "height", "main_chain", "mrkl_root", "n_tx", "next_block",
+                       "prev_block", "address", "total_received", "total_sent", "final_balance"]:
+                text.insert("1.0", f"{key.title().replace('_', ' ')}: {value}\n")
 
-        text.insert("1.0", "\n".join(lines))
 
 if __name__ == "__main__":
     root = tk.Tk()
